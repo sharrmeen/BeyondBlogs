759de74ebf830e393f0960a9af8677d05ddbf6f9
Sharmeen <sharmeenk2020@gmail.com>
2026-01-05 01:55:23 +0530

    Updated scraper logic : fetches five oldest links,their content , and stores them in the db

--- backend/scripts/scrapeOldest.js
+++ backend/scripts/scrapeOldest.js
@@ -1,4 +1,5 @@
 import puppeteer from "puppeteer";
+import { Blog } from "../models/Blogs.js";
 
 export const scrapedOldestArticles = async (lastPageUrl) => {
     
@@ -44,8 +45,48 @@ export const scrapedOldestArticles = async (lastPageUrl) => {
         const finalTargets = collectedLinks.slice(0, target);
         console.log("Final 5 Oldest URLs:", finalTargets);
 
+        // function to navigate to each url and collect the data according to the model
+
+        for (const url of finalTargets) {
+
+            const exists = await Blog.exists({ url });
+            if (exists) {
+                console.log(`- Skipping: Already processed ${url}`);
+                continue;
+            }
+
+            const articlePage = await browser.newPage();
+            
+            await articlePage.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
+
+            try {
+                await articlePage.goto(url, { waitUntil: 'domcontentloaded', timeout: 45000 });
+
+                const articleData = await articlePage.evaluate((link) => ({
+                    title: document.querySelector('h1.elementor-heading-title')?.innerText.trim() || "Untitled",
+                    content: document.querySelector('.elementor-widget-theme-post-content')?.innerText.trim() || "",
+                    url: link,
+                    is_updated: false
+                }), url);
+
+               
+                if (!articleData.content) {
+                    console.warn(`! Warning: No content found for ${url}`);
+                    continue; 
+                }
+
+                await Blog.create(articleData);
+                console.log(`Saved to db: ${articleData.title}`);
+
+            } catch (err) {
+                console.error(`ailed to scrape ${url}:`, err.message);
+            } finally {
+                await articlePage.close();
+            }
+        }
+
         await browser.close();
-        return finalTargets;
+        // return finalTargets;
 
     } catch (error) {
         console.error("Scraping failed:", error.message);
