ad2bf7cb02a768a06058377f9f4a2257d1152e87
Sharmeen <sharmeenk2020@gmail.com>
2026-01-07 02:25:55 +0530

    deployment updates

--- backend/services/articleProcessor.js
+++ backend/services/articleProcessor.js
@@ -1,5 +1,4 @@
 import 'dotenv/config';
-import fetch from 'node-fetch';
 import puppeteer from 'puppeteer';
 import { rewriteArticle } from './llmService.js';
 import { Blog } from '../models/Blogs.js';
